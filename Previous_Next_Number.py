f6f1ef901c90904ca48ab2a36723701bd504a2e1
falverca <falverca.coding@gmail.com>
2023-07-12 20:56:49 -0300

    Program that asks for a number and shows the previous and the next numbers

---
+++ Previous_Next_Number.py
@@ -0,0 +1,5 @@
+# Build a program that asks for a number and shows the previous and the next number
+
+n = int(input("Insert a number: "))
+
+print(f"The number before {n} is {n - 1} and the number after {n} is {n + 1}")
