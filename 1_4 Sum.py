d2367ea4c62d8d01d81fa15a648500e7df05176b
falverca <falverca.coding@gmail.com>
2023-07-12 21:06:37 -0300

    Sum of two numbers

---
+++ 1_4 Sum.py
@@ -0,0 +1,8 @@
+# Build a program that asks for two numbers and shows the sum of them
+
+n1 = float(input("Insert a number: "))
+n2 = float(input("Insert another number: "))
+
+sum = n1 + n2
+
+print(f"The sum is: {sum}.")
