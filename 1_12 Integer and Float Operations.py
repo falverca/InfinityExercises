64df3a4ac952a6b5905c96ea7474f6d170c96624
falverca <falverca.coding@gmail.com>
2023-07-12 22:01:37 -0300

    Operations with integers and real numbers

---
+++ 1_12 Integer and Float Operations.py
@@ -0,0 +1,12 @@
+# Build a program that asks for two integers and one real number. Calculate and show:
+# the product of the double of the first number with half of the second one.
+# the sum of triple of the first number with the third number.
+# the third number to the power of 3.
+
+int1 = int(input("Insert an integer: "))
+int2 = int(input("Insert another integer: "))
+real3 = float(input("Insert a real number: "))
+
+print(f"The product of the double of the first number with half of the second one is equal to {int1 * 2 * (int2 / 2)}.")
+print(f"The sum of triple of the first number with the third number is equal to {(int1 * 3) + real3}.")
+print(f"The third number to the power of 3 is equal to {real3 ** 3}.")
--- 1_8 Circle_area.py
+++ 1_8 Circle_area.py
@@ -1,6 +1,6 @@
 # Build a program that asks for the radius of a circle (cm) and calculates and shows its area. Pi = 3
 
-radius = float(input("What's the radius in cm: "))
+radius = float(input("What's the radius of the circle in cm: "))
 pi = 3
 
 area = pi * (radius ** 2)
